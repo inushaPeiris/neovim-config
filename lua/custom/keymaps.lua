03cd49fff6cfaac58b5fda486be395af7e0599a7
Inusha Peiris <inusha@imperiallearning.co.uk>
2025-04-03 11:31:47 +0530

    Initial commit - Neovim config

---
+++ lua/custom/keymaps.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<M-h>", "<Cmd>BufferLineCyclePrev<CR>", { silent = true, desc = "Previous Buffer" })
+vim.keymap.set("n", "<M-l>", "<Cmd>BufferLineCycleNext<CR>", { silent = true, desc = "Next Buffer" })
